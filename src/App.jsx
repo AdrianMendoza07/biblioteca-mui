1e9f71380cb3155e4b6767fd05c6501089e6ead5
AdrianMendoza07 <adrian25336@gmail.com>
2023-11-08 20:03:34 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './pages/login'
+
+function App() {
+  
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/'  Component={Login}/>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
